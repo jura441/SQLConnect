8e7737e95cae4c07802030ac36ca3bf034ea53bd
Юрий Иванов <jura401@mail.ru>
2023-01-11 21:22:18 +0300

    Добавьте файлы проекта.

---
+++ SQLConnect/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLConnect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = null;
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Library; Integrated Security = True";
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            Console.WriteLine("подключение было открыто");
+            //connection.ConnectionString = connectionString;
+            connection.Close();
+            Console.WriteLine("подключение было закрыто");
+
+
+            /*Connect Timeout = 30; 
+            Encrypt = False; 
+            TrustServerCertificate = False; 
+            ApplicationIntent = ReadWrite;
+            MultiSubnetFailover = False*/
+        }
+    }
+}
